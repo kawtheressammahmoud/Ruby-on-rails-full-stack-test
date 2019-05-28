10d415d6cc1e9562431ca1cc9cd8f6baa9019867
kawther essam <kawther.esam14@gmail.com>
2019-05-28 23:26:45 +0200

    Update pricing_algorithm.rb

--- pricing_algorithm.rb
+++ pricing_algorithm.rb
@@ -1,88 +1,72 @@
-if brand.rating = high
-  if price_reference < 15
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.75) * 1.1
-    price_original = price_suggestion
-  elsif price_reference < 25
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.775) * 1.1
-    price_original = price_suggestion
-  elsif price_reference < 50
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.8) * 1.1
-    price_original = price_suggestion
-  elsif price_reference < 75
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.825) * 1.1
-    price_original = price_suggestion
-  elsif price_reference < 100
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.85) * 1.1
-    price_original = price_suggestion
-  elsif price_reference < 200
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.875) * 1.1
-    price_original = price_suggestion
-  elsif price_reference < 400
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.9) * 1.1
-    price_original = price_suggestion
-  elsif price_reference < 600
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.925) * 1.1
-    price_original = price_suggestion
+brand_rating = brand.rating
+base_price = price_reference - product_database.shipping_cost
+arr = []
+(0.75..0.95).step(0.025).each do |i|
+  # print the results for testing 
+  p i.to_s + "_product_price:"
+  p product_price = (base_price * i)
+  arr << product_price
+  # print the array for testing 
+  p arr
+end 
+if brand_rating = high
+    if price_reference < 15
+      price_original = arr[0] * 1.1
+    elsif price_reference < 25
+      price_original = arr[1] * 1.1
+    elsif price_reference < 50
+      price_original = arr[2] * 1.1
+    elsif price_reference < 75
+      price_original = arr[3] * 1.1
+    elsif price_reference < 100
+      price_original = arr[4] * 1.1
+    elsif price_reference < 200
+      price_original = arr[5] * 1.1
+    elsif price_reference < 400
+      price_original = arr[6] * 1.1
+    elsif price_reference < 600
+      price_original = arr[7] * 1.1
+    else
+      price_original = arr[8] * 1.1
+    end 
+  elsif brand_rating = low
+    if price_reference < 15   
+      price_original = arr[0] * 0.9
+    elsif price_reference < 25   
+      price_original = arr[1] * 0.9
+    elsif price_reference < 50  
+      price_original = arr[2] * 0.9
+    elsif price_reference < 75     
+      price_original = arr[3] * 0.9
+    elsif price_reference < 100       
+      price_original = arr[4] * 0.9
+    elsif price_reference < 200       
+      price_original = arr[5] * 0.9
+    elsif price_reference < 400       
+      price_original = arr[6] * 0.9
+    elsif price_reference < 600   
+      price_original = arr[7] * 0.9
+    else 
+      price_original = arr[8] * 0.9
+    end 
   else
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.95) * 1.1
-    price_original = price_suggestion
-elsif brand.rating = low
-  if price_reference < 15
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.75) * 0.9
-    price_original = price_suggestion
-  elsif price_reference < 25
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.775) * 0.9
-    price_original = price_suggestion
-  elsif price_reference < 50
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.8) * 0.9
-    price_original = price_suggestion
-  elsif price_reference < 75
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.825) * 0.9
-    price_original = price_suggestion
-  elsif price_reference < 100
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.85) * 0.9
-    price_original = price_suggestion
-  elsif price_reference < 200
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.875) * 0.9
-    price_original = price_suggestion
-  elsif price_reference < 400
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.9) * 0.9
-    price_original = price_suggestion
-  elsif price_reference < 600
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.925) * 0.9
-    price_original = price_suggestion
-  else
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.95) * 0.9
-    price_original = price_suggestion
-else
-  if price_reference < 15
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.75) * 1
-    price_original = price_suggestion
-  elsif price_reference < 25
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.775) * 1
-    price_original = price_suggestion
-  elsif price_reference < 50
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.8) * 1
-    price_original = price_suggestion
-  elsif price_reference < 75
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.825) * 1
-    price_original = price_suggestion
-  elsif price_reference < 100
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.85) * 1
-    price_original = price_suggestion
-  elsif price_reference < 200
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.875) * 1
-    price_original = price_suggestion
-  elsif price_reference < 400
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.9) * 1
-    price_original = price_suggestion
-  elsif price_reference < 600
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.925) * 1
-    price_original = price_suggestion
-  else
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.95) * 1
-    price_original = price_suggestion
-end
-
-
-
+    if price_reference < 15
+      price_original = arr[0]
+    elsif price_reference < 25
+      price_original = arr[1]
+    elsif price_reference < 50 
+      price_original = arr[2]
+    elsif price_reference < 75        
+      price_original = arr[3]
+    elsif price_reference < 100         
+      price_original = arr[4]
+    elsif price_reference < 200       
+      price_original = arr[5]
+    elsif price_reference < 400        
+      price_original = arr[6]
+    elsif price_reference < 600        
+      price_original = arr[7]
+    else       
+      price_original = arr[8]
+    end 
+  end
