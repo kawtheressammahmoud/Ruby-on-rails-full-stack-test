b3f4d10aa497047b5b9f2c25ef8d69081ce2f5ec
kawther essam <kawther.esam14@gmail.com>
2019-06-12 22:42:39 +0200

    Update pricing_algorithm_enhancement.rb

--- pricing_algorithm_enhancement.rb
+++ pricing_algorithm_enhancement.rb
@@ -1,6 +1,12 @@
 
-brand_rating = brand.rating
+
+# as the price name - product_database.shipping_cost was repeated in 
+# multiple lines and used in multiple calcuations i put it in a variable called base_price
 base_price = price_reference - product_database.shipping_cost
+# and the same for the brand.rating
+brand_rating = brand.rating
+# an empty array that stores the result of the below method which generate an array 
+# of numbers from 0.75 to 0.95 with step 0.025
 arr = []
 (0.75..0.95).step(0.025).each do |i|
   # print the results for testing 
@@ -10,6 +16,8 @@ arr = []
   # print the array for testing 
   p arr
 end 
+# then after creating the array we will take its output which will be (base_price * i) 
+# where i is the value from 0.75 to 0.95 with step 0.025
 if brand_rating = high
     if price_reference < 15
       price_original = arr[0] * 1.1
